b97e767837d523bc8df1ac8c56903ab100d986a6
lgdoliveira <lgdoliveira.ciet@rd.com.br>
2022-04-25 20:52:52 -0300

    Primeiro commit

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import LoginPage from './pages/LoginPages';
+import HomePage from './pages/HomePages';
+import { AuthProvider, AuthContext } from "./contexts/auth";
+
+const AppRoutes = () => {
+  const Private = ({children}) => {
+    const { authenticated, loading } = useContext(AuthContext);
+
+    if(loading){
+        return <div className="loading">Carregando...</div>;
+    }
+
+    if(!authenticated){
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+  }
+  
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<Private><HomePage /></Private>} />
+          <Route exact path="/login" element={<LoginPage />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+};
+
+export default AppRoutes;
